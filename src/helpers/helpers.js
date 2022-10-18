bac94c434192668080059c80613a53f0e4b6c829
moschidisg <mosxidismosxidis@gmail.com>
2022-10-18 13:30:10 +0300

    add helpers folder with ship info

---
+++ src/helpers/helpers.js
@@ -0,0 +1,17 @@
+const SHIP_TYPES = [
+    'carrier',
+    'battleship',
+    'cruiser',
+    'submarine',
+    'destroyer',
+];
+
+const SHIP_LENGTHS = {
+    carrier: 5,
+    battleship: 4,
+    cruiser: 3,
+    submarine: 3,
+    destroyer: 2,
+};
+
+export { SHIP_TYPES, SHIP_LENGTHS };
