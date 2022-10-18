3e81790f843f1ba6cacfd7604856c324c1e8fbbe
moschidisg <mosxidismosxidis@gmail.com>
2022-10-18 13:28:45 +0300

    gameboard refactoring

---
+++ src/__tests__/gameboard.test.js
@@ -0,0 +1,33 @@
+import { Gameboard } from '../gameboard';
+
+test('fails to place ship', () => {
+    const gb = new Gameboard();
+    gb.placeShip(2, 3, 2, 'h');
+    expect(gb.placeShip(2, 3, 2, 'h')).toBeFalsy();
+});
+test('loses', () => {
+    const gb = new Gameboard();
+    gb.placeShip(1, 1, 2, 'h');
+    gb.receiveAttack(1, 1);
+    gb.receiveAttack(1, 2);
+    expect(gb.hasLost()).toBeTruthy();
+});
+test('multiple ships hit but not lost', () => {
+    const gb = new Gameboard();
+    gb.placeShip(2, 2, 2, 'h');
+    gb.placeShip(3, 3, 2, 'v');
+    gb.receiveAttack(2, 2);
+    gb.receiveAttack(2, 3);
+    gb.receiveAttack(3, 3);
+    expect(gb.hasLost()).toBeFalsy();
+});
+test('all ships hit and lost', () => {
+    const gb = new Gameboard();
+    gb.placeShip(2, 2, 2, 'h');
+    gb.placeShip(3, 3, 2, 'v');
+    gb.receiveAttack(2, 2);
+    gb.receiveAttack(2, 3);
+    gb.receiveAttack(3, 3);
+    gb.receiveAttack(4, 3);
+    expect(gb.hasLost()).toBeTruthy();
+});
---
+++ src/gameboard.js
@@ -0,0 +1,92 @@
+import { Ship } from './ship';
+
+const Gameboard = () => {
+    let board = [];
+    let missed = [];
+    let dimensions = 10;
+
+    for (let i = 0; i < dimensions; i++) {
+        board[i] = [];
+        missed[i] = [];
+        for (let j = 0; j < dimensions; j++) {
+            board[i].push(0);
+            missed[i].push(0);
+        }
+    }
+
+    const placeShip = (row, col, length, dir) => {
+        let ship = Ship(length);
+
+        if (isValidPlacement(row, col, ship.length, dir)) {
+            if (dir === 'h') {
+                for (let i = col; i < col + length; i++) {
+                    board[row][i] = ship;
+                }
+            } else {
+                for (let i = row; i < row + ship.length; i++) {
+                    board[i][col] = ship;
+                }
+            }
+        }
+    };
+
+    const isValidPlacement = (row, col, length, dir) => {
+        if (
+            row < 0 ||
+            row > dimensions - 1 ||
+            col < 0 ||
+            col > dimensions - 1
+        ) {
+            return false;
+        }
+
+        if (dir === 'h') {
+            if (col + length - 1 > dimensions) {
+                return false;
+            }
+
+            for (let i = 0; i < length; i++) {
+                if (board[row][col + i] != 0) {
+                    return false;
+                }
+            }
+            return true;
+        } else if (dir === 'v') {
+            if (row + length - 1 > dimensions) {
+                return false;
+            }
+
+            for (let i = 0; i < length; i++) {
+                if (board[row + i][col] != 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    };
+
+    const receiveAttack = (row, col) => {
+        if (board[row][col] == 0) {
+            missed[row][col] = 1;
+        } else {
+            board[row][col].hit();
+        }
+    };
+
+    const hasLost = () => {
+        for (let i = 0; i < dimensions; i++) {
+            for (let j = 0; j < dimensions; j++) {
+                if (board[i][j] != 0) {
+                    if (!board[i][j].isSunk()) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    };
+
+    return { placeShip, receiveAttack, hasLost, missed };
+};
+
+export { Gameboard };
